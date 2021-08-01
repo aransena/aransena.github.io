892e7da417fcedb23289db7e8f8a8b6ecfd65c4e
aransena <aransena@gmail.com>
2021-08-01 01:52:06 +0100

    Update inter.js

--- static/inter.js
+++ static/inter.js
@@ -7,13 +7,23 @@ function setup(){
 }
 
 function draw(){	
-	let r, g, b;
+	let r, g, b, ro, go, bo;
 	r = 255*(sin(Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
 	g = 255*(sin(2*Math.PI*.33 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
 	b = 255*(sin(2*Math.PI*.66 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
 	background(r, g, b);
+	i = 1;
+	ro = 255*(sin(Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
+	go = 255*(sin(2*Math.PI*.33 + 2*Math.PI/12 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
+	bo = 255*(sin(2*Math.PI*.66 + 2*Math.PI/12 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
+	stroke(255-ro, 255-go, 255-bo);
+	fill(255-ro, 255-go, 255-bo);
+	text("Aran Sena", windowWidth/2 + i*10, windowHeight/2 + i*10)
+
+	i = 0;
 	stroke(255-r, 255-g, 255-b);
 	fill(255-r, 255-g, 255-b);
-	text("Aran Sena", windowWidth/2, windowHeight/2)
+	text("Aran Sena", windowWidth/2 + i*10, windowHeight/2 + i*10)
+
 
 }
