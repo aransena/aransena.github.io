ebf8386135bfa267a115eebbf3b5dc08c7591b89
aransena <aransena@gmail.com>
2021-08-01 01:37:23 +0100

    Update inter.js

--- static/inter.js
+++ static/inter.js
@@ -1,6 +1,9 @@
 function setup(){
 	frameRate(60);
 	createCanvas(windowWidth, windowHeight);
+	textFont(Helvetica);
+	textSize(width / 3);
+	textAlign(CENTER, CENTER);
 }
 
 function draw(){
@@ -8,5 +11,8 @@ function draw(){
 	r = 255*(sin(Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
 	g = 255*(sin(2*Math.PI*.33 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
 	b = 255*(sin(2*Math.PI*.66 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
+	stroke(255-r, 255-g, 255-b);
+	fill(255-r, 255-g, 255-b);
+	text("Aran Sena", windowWidth/2, windowHeight/2)
 	background(r, g, b);
 }
