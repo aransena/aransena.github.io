903814b3b0245a3dce5078ea523255466023f5ec
aransena <aransena@gmail.com>
2021-08-01 01:55:44 +0100

    Update inter.js

--- static/inter.js
+++ static/inter.js
@@ -7,18 +7,18 @@ function setup(){
 }
 
 function draw(){	
-	let r, g, b, ro, go, bo;
+	let r, g, b, ro, go, bo, offset;
 	r = 255*(sin(Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
 	g = 255*(sin(2*Math.PI*.33 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
 	b = 255*(sin(2*Math.PI*.66 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
 	background(r, g, b);
-	i = 1;
+	offset = sin((frameCount/1000)%60)*0.5 + 1.5;
 	ro = 255*(sin(Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
-	go = 255*(sin(2*Math.PI*.33 + 2*Math.PI/12 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
-	bo = 255*(sin(2*Math.PI*.66 + 2*Math.PI/12 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
+	go = 255*(sin(2*Math.PI*.33 + 2*Math.PI/9 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
+	bo = 255*(sin(2*Math.PI*.66 + 2*Math.PI/9 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
 	stroke(255-ro, 255-go, 255-bo);
 	fill(255-ro, 255-go, 255-bo);
-	text("Aran Sena", windowWidth/2 + i*10, windowHeight/2 + i*10)
+	text("Aran Sena", windowWidth/2 + offset*10, windowHeight/2 + offset*10)
 
 	i = 0;
 	stroke(255-r, 255-g, 255-b);
