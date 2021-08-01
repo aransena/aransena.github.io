941354dabf45d4cb2d01596e5a334a8cd1663728
aransena <aransena@gmail.com>
2021-08-01 01:27:54 +0100

    Update inter.js

--- static/inter.js
+++ static/inter.js
@@ -4,6 +4,9 @@ function setup(){
 }
 
 function draw(){
-	
-	background(255*(sin(Math.PI*2*(frameCount/60))*0.5 + 0.5));
+	let r, g, b;
+	r = 255*(sin(Math.PI*2*(frameCount/60))*0.5 + 0.5);
+	g = 255*(sin(Math.PI*.33 + Math.PI*2*(frameCount/60))*0.5 + 0.5);
+	b = 255*(sin(Math.PI*.66 + Math.PI*2*(frameCount/60))*0.5 + 0.5);
+	background(r, g, b);
 }
