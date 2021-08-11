cc6424d16322ac823a7910df130aa08db2ec74d2
Aran Sena <aransena@gmail.com>
2021-08-11 23:08:52 +0100

    bubbles

---
+++ local/local_server.py
@@ -0,0 +1,16 @@
+# local flask server for testing before pushing
+
+# Basic flask + p5.js server, using getting started examples from p5.js and flask
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='..', static_folder='../static')
+
+@app.route('/')
+def home(name=None):
+    return render_template('index.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run()
--- static/inter.js
+++ static/inter.js
@@ -1,33 +1,70 @@
+var t;
 function setup(){
-	frameRate(60);
+	frameRate(120);
+	// colorMode(HSB, 100);
+	background(255, 255, 255);
 	createCanvas(windowWidth, windowHeight);
-// 	textFont(Helvetica);
 	textSize(windowWidth / 10);
 	textAlign(CENTER, CENTER);
+	t = 0;
+
 }
 
-function draw(){	
-	let r, g, b, ro, go, bo, offsetx, offsety;
-	r = 255*(sin(Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
-	g = 255*(sin(2*Math.PI*.33 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
-	b = 255*(sin(2*Math.PI*.66 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
-	background(r, g, b);
-	offsetx = (sin(Math.PI*2*(frameCount/1000)%60)*0.5 + 0.5)*0.75;
-	offsety = (sin(Math.PI*2*(frameCount/1000)%60)*0.5 + 0.5)*0.75;
-	ro = 255*(sin(Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
-	go = 255*(sin(2*Math.PI*.33 + 2*Math.PI/9 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
-	bo = 255*(sin(2*Math.PI*.66 + 2*Math.PI/9 + Math.PI*2*((frameCount/1000)%60))*0.5 + 0.5);
-	stroke(255-ro, 255-go, 255-bo);
-	fill(255-ro, 255-go, 255-bo);
-	let offsetsize = windowWidth/10; //((5-offsetx)/5) * (windowWidth / 10);
-	textSize(offsetsize);
-	text("Aran Sena", windowWidth/2 + offsetx*10, windowHeight/2 + offsety*10)
+function draw(){
+	clear();
 
-	i = 0;
-	stroke(255-r, 255-g, 255-b);
-	fill(255-r, 255-g, 255-b);
-	textSize(windowWidth / 10);
-	text("Aran Sena", windowWidth/2 + i*10, windowHeight/2 + i*10)
+	let x, y, offset_x, offset_y, amplitude_x, amplitude_y, frequency_x, frequency_y, phase_x, phase_y;
+	let R = random(1,10)
+	for(let i = 0; i < 1000; i++){
+		strokeWeight(10*(0.5+0.5*sin(i/1000)));
+
+		t = t + 0.000001;
+		offset_x = windowWidth/2;
+		offset_y = windowHeight/2;
+		amplitude_x = 1000;
+		amplitude_y = windowHeight;
+		frequency_x = i/100000;//0.001;
+		frequency_y = 0.01;
+		phase_x = i%101;
+		phase_y = 0;
+		x = offset_x + amplitude_x * sin(2 * Math.PI * frequency_x * (i+t) + phase_x);
+
+		y = offset_y + amplitude_y * cos(2 * Math.PI * frequency_y * (i+t) + phase_y);
+		// for(let r = 1; r<R; r++) {
+			x = offset_x + amplitude_x * sin(2 * Math.PI * frequency_x  * x + phase_x);
+			y = offset_y + amplitude_y * cos(2 * Math.PI * frequency_y * y + phase_y);
+		// }
+		stroke(240,240,240);
+		fill(240, 240,240);
+		point(x,y);
+	}
+
+
+	// let r, g, b, ro, go, bo, offsetx, offsety, M;
+	// M = 100;
+	// for(let n = 0; n < M; n++) {
+	// 	// strokeWeight(10);
+	//
+	//
+	// 	let N = 30;
+	// 	let freq, amp, phase, off, wave, t, T, step, start;
+	// 	freq = 0.001 + n/10000;
+	// 	amp = 40.0;
+	// 	wave = 1.0;
+	// 	off = ((n*1.5)*(windowHeight-amp*2)) / M;
+	// 	phase = n*10;//0.0;
+	// 	T = (n%101+1)*100.0;
+	// 	step = windowWidth / T;
+	// 	for (let i = 0; i < N; i++) {
+	// 		stroke(i+ 0.1 * n % 100, 80, 100);
+	// 		fill(i + 0.1 * n % 20, 80)
+	// 		t = ((frameCount) % windowWidth) * step - i * step;
+	// 		let x = t % (windowWidth + N);//step*(frameCount-i)%step;
+	// 		let y = off + amp * sin(2 * Math.PI * freq * t + phase);
+	// 		strokeWeight(10 - (i / N) * 10);
+	// 		point(x, y);
+	// 	}
+	// }
 
 
 }
