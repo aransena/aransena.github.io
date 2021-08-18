750eedda0079122b6f047296316bc3c816f3a809
Aran Sena <aransena@gmail.com>
2021-08-18 12:26:57 +0100

    ...

--- static/index.js
+++ static/index.js
@@ -1,3 +1,4 @@
+let rand = [];
 function setup() {
     	frameRate(120);
 
@@ -16,26 +17,41 @@ function setup() {
         e.position(windowWidth/15, windowHeight/20+80);
         let f = createA('https://scholar.google.co.uk/citations?user=PmpjruUAAAAJ&hl=en#','google scholar');
         f.position(windowWidth/15, windowHeight/20+100);
-
+        for(let i = 0; i<50; i++) {
+            rand.push(Math.floor(Math.random() * 1000));
+        }
 
 }
 
 function draw(){
     clear()
 
-        for(let i = 0; i<(650); i++) {
-            let t = i;
-            strokeWeight(8 * (0.6 + 0.5 * sin(t / 2)));
-            stroke((0.5 + sin(0.5 * t / 5 + t/200)*cos(t/10)) * 255, 0, 240);
-            fill(0, 240, 240);
-            let x = cos(t / 100 + frameCount/180 * sin(frameCount/1000)) * windowWidth / 2 * 0.4+ windowWidth / 2 + 80 * sin(t / 100 + frameCount/100)+50*sin(t/7+2*sin(t/100))+10*cos(t/110);// * sin(frameCount/10);
-            let y = sin(t / 100 + frameCount/900) * windowHeight / 2 * 0.4 + windowHeight / 2 + 80 * cos(t / 10) * sin(t / 10 + frameCount/110);
-            point(x, y);
-            // for(let j=0; j< 1000; j += 100) {
-            //     point(j * (windowWidth - x) / windowWidth, j * (windowHeight - y) / windowHeight);
-            // }
-            // text("Hello",y,x)
+        for(let j = 0; j< rand.length; j++) {
+            let N = 20+floor(rand[j]/1000*40);
+            for (let i = rand[j]; i < rand[j] + N; i++) {
+                let t = frameCount + i;
+                strokeWeight(N/6 * (1.5 + 0.5 * sin(t / 2)));
+                stroke(min(N,(0.5 + sin(0.5 * t / 5 + t / 200)) * cos(t / 10)) * 255, 255* rand[j]/1000, 230-N);
+                fill(0, 240, 240);
+                let x = cos(rand[j] + t / 100 + t / (10+N*10) * sin((t / (1000+N*3))) + N/50) * windowWidth / 2 * 0.4 + windowWidth / 2 + windowWidth / windowHeight * sin(t / (100+N*100) + rand[j]/100+ (i/N)*2*Math.PI*N/500) + windowHeight / windowWidth * 100 * sin(t / (70+N*10+ (i/N)*2*Math.PI*N/500) + 2 * rand[j]/100*sin(t / 2000)) + windowHeight / windowWidth * 100 * cos(t / (339+N));// * sin(frameCount/10);
+                let y = sin(rand[j]+ t / (100-N*10) ) * windowHeight / 2 * 0.4 + windowHeight / 2 + windowWidth/windowHeight/(rand[j]+1) * sin(t/300) * windowHeight/100 * cos(t / 100) * sin(t / 1000 + (i/N)*2*Math.PI*N/500) + t/100*cos(t/(rand[j]+1+N*10));
+                point(x, y);
+                // for(let j=0; j< 1000; j += 100) {
+                //     point(j * (windowWidth - x) / windowWidth, j * (windowHeight - y) / windowHeight);
+                // }
+                // text("Hello",y,x)
+            }
         }
+
+        // for(let i = rand; i<rand+N; i++) {
+        //     let t = frameCount+i;
+        //     strokeWeight(15 * (1.5 + 0.5 * sin(t / 2)));
+        //     stroke((0.5 + sin(0.5 * t / 5 + t/200)*cos(t/10)) * 255, 0, 240);
+        //     fill(0, 240, 240);
+        //     let x = cos(t / 100 + t/180 * sin((t/1000))) * windowWidth / 2 * 0.4+ windowWidth / 2 + windowWidth/windowHeight * sin(t / 100 )+windowHeight/windowWidth*100*sin(t/70+2*sin(t/2000))+windowHeight/windowWidth*100*cos(t/330);// * sin(frameCount/10);
+        //     let y = sin(t / 100) * windowHeight / 2 * 0.4 + windowHeight / 2;// + windowWidth/windowHeight * sin(t/30) * windowHeight/10 * cos(t / 100 + t%N/50) * sin(t / 100 + t/110) + 10*t/100*cos(t/10);
+        //     point(x, y);
+        // }
 
 
 
