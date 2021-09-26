bd4115929c8c718ac9d8fded660c710b7e90747a
aransena <aransena@gmail.com>
2021-09-26 14:54:47 +0100

    Create qr2.js

---
+++ static/qr2.js
@@ -0,0 +1,12 @@
+let img1;
+
+function setup() {
+    	createCanvas(windowWidth, windowHeight);
+        img1 = loadImage('static/QR2.png'); // Load the image
+        document.body.style.overflow = 'hidden';
+}
+
+function draw(){
+image(img1, -30, -30, img1.width, img1.height);
+
+}
