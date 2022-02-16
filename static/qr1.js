17c42f6c75ca2c5c35a9444e56a5f074584f1cdf
aransena <aransena@gmail.com>
2022-02-16 16:34:40 +0000

    Update qr1.js

--- static/qr1.js
+++ static/qr1.js
@@ -2,12 +2,20 @@ let img1;
 
 function setup() {
     	createCanvas(windowWidth, windowHeight);
-        img1 = loadImage('static/QR1.png'); // Load the image
+        //img1 = loadImage('static/QR1.png'); // Load the image
+        var qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: "http://jindo.dev.naver.com/collie",
+        width: document.documentElement.clientWidth*0.25,
+        height: document.documentElement.clientWidth*0.25,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
         document.body.style.overflow = 'hidden';
 }
 
 function draw(){
 //image(img1, 0, 0, img1.width, img1.height);
-    image(img1, 0, 0, document.documentElement.clientWidth*0.25, document.documentElement.clientWidth*0.25 * img1.width/img1.height);
+    //image(img1, 0, 0, document.documentElement.clientWidth*0.25, document.documentElement.clientWidth*0.25 * img1.width/img1.height);
 
 }
